a41e382755db52b67947d8c6813f33eb1bdb147b
moses mccabe <moses.peace.mccabe@gmail.com>
2023-08-01 17:46:00 -0700

    JPG to PNG converter python script

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+print(image_folder, output_folder)
+
+# check is new/ exists, if not create
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# loop through Pokedex,
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    # convert images to png
+    # save to the new folder
+    img.save(fp=f'{output_folder}{clean_name}.png', format='png')
+
+print('done')
